66b95dc10b3e2ab513e20c547ab7d2f9275f8f02
Katabela <90431458+Katabela@users.noreply.github.com>
2021-12-10 23:52:24 -0700

    files uploaded

---
+++ javascripts/main.js
@@ -0,0 +1,32 @@
+// Required by Webpack - do not touch
+require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+require.context('../stylesheets/', true, /\.(css|scss)$/i)
+
+//TODO - Your ES6 JavaScript code (if any) goes here
+import * as fontawesome from "@fortawesome/fontawesome-free/js/all"
+
+CSS.registerProperty({
+    name: '--ang', 
+    syntax: '<angle>', 
+    initialValue: '0deg', 
+    inherits: true
+  });
+  
+
+$(document).on("click", ".browse", function() {
+  var file = $(this).parents().find(".file");
+  file.trigger("click");
+});
+$('input[type="file"]').change(function(e) {
+  var fileName = e.target.files[0].name;
+  $("#file").val(fileName);
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    // get loaded data and render thumbnail.
+    document.getElementById("preview").src = e.target.result;
+  };
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});
